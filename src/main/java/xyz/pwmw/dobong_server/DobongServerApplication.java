c7abab96e91970d0731d06e0f458a5b7dc917e18
umustmw <umustmw@gmail.com>
2025-03-02 15:36:57 +0900

    first commit

---
+++ src/main/java/xyz/pwmw/dobong_server/DobongServerApplication.java
@@ -0,0 +1,13 @@
+package xyz.pwmw.dobong_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DobongServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DobongServerApplication.class, args);
+    }
+
+}
